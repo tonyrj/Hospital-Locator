021d5b06c41047ab618b7ede5a9df83a3c53fa1f
vaseem14 <36194950+vaseem14@users.noreply.github.com>
2019-08-04 23:11:37 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,64 @@
+$( document ).ready(function() {
+// lan and long
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+} else {
+    alert('It seems like Geolocation, which is required for this page, is not enabled in your browser. Please use a browser which supports it.');
+    window.stop() 
+
+}
+
+function successFunction(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    localStorage.setItem("lat",lat);
+    localStorage.setItem("long",long);
+    console.log('Your latitude is :'+lat+' and longitude is '+long);
+    return lat;
+}
+function errorFunction(position){
+    window.stop() 
+
+    console.log("shit");
+}});
+
+            // var sydney = new google.maps.LatLng(12.850412, 80.2241513);
+var map;
+var service;
+var infowindow;
+
+function initialize() {
+  var location = new google.maps.LatLng(localStorage.getItem("lat"),localStorage.getItem("long"));
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: location,
+      zoom: 15
+    });
+
+  var request = {
+    location: location,
+    radius: '2000',
+    type: ['hospital']
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+function createMarker(place) {
+
+new google.maps.Marker({
+    position: place.geometry.location,
+    map: map,
+    // icon: place.icon,
+    animation:google.maps.Animation.Drop
+});
+}
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+    //   console.log(place);
+      createMarker(results[i]);
+    }
+  }
+}
